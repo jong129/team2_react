eae3dbdc16edc2bf688db88a70589a6fe1bff7df
jongho <rwg1254@naver.com>
2025-11-28 10:48:30 +0900

    team2

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { getCopyright } from './components/Tool';
+import Home from './components/Home'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <div style={{width: '100%'}}>
+        2조 React Frontend project
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+
+        <div className='copyright'>{getCopyright()}</div>      
+      </div>
+    </BrowserRouter>
+
+
+  )
+}
+
+export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <h5>Team2</h5>
+  )
+}
+
+export default Home
