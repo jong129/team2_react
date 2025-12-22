f7c19fdfb006d44e3075d209ed3a7c36a0763aaf
limyonghyuk <jordanlim9808@gmail.com>
2025-12-22 17:37:46 +0900

    Home.jsx 제작

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import { getCopyright } from './components/Tool';
 import Home from './components/Home'
